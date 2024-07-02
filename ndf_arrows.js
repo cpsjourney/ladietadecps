911682013e4e614b49f88ad179e0fd26263e6dbc
cpsjourney <121058936+cpsjourney@users.noreply.github.com>
2024-07-02 22:08:52 +0200

    ndf_arrows.js

--- ndf_arrows.js
+++ ndf_arrows.js
@@ -51,20 +51,20 @@ titolocontenuto_leftarrow_1.onclick = function() {
     if (titolocontenuto_colazione_1.innerHTML === "COLAZIONE") {
         titolocontenuto_colazione_1.innerHTML = "CENA";
         contenuto_default_1.innerHTML = `
-            - BROCCOLI 150g | kcal: 52, prot: 5g, carbo: 10g, fibre: 5g<br>
+            - CAROTE 150g | kcal: 62, prot: 2g, carbo: 15g, fibre: 4g<br>
             - PETTO DI POLLO 200g | kcal: 330, prot: 62g, grassi: 7g<br>
             - 3 CUCCHIAI D'OLIO EVO | kcal: 390, grassi: 45g<br>
             - 1 YOGURT 125g | kcal: 90, prot: 5g, carbo: 10g, grassi: 3g<br>
-            (_TOT_ KCAL: 862, PROT: 72g, CARBO: 20g, GRASSI: 55g, FIBRE: 5g)
+            (_TOT_ KCAL: 872, PROT: 69g, CARBO: 25g, GRASSI: 55g, FIBRE: 4g)
         `;
     } else if (titolocontenuto_colazione_1.innerHTML === "CENA") {
         titolocontenuto_colazione_1.innerHTML = "MERENDA";
         contenuto_default_1.innerHTML = `
             - 1 YOGURT 125g (2% grassi) | kcal: 77, prot: 10g, carbo: 15g, grassi: 3g<br>
-            - 2 ARANCE 230g(no buccia) | kcal: 100, prot: 2g, carbo: 30g, fibre: 5g<br>
+            - 2 PESCHE 280g | kcal: 110, prot: 2g, carbo: 26g, fibre: 4g<br>
             - 6 BISCOTTI (tremarie) 90g | kcal: 460, prot: 6g, carbo: 60g, grassi: 20g, fibre: 2g<br>
             - 1 INTEGRATORE PROT 1misurino | kcal: 120, prot: 25g, carbo: 3g, grassi: 1g<br>
-            (_TOT_ KCAL: 757, PROT: 43g, CARBO: 108g, GRASSI: 24g, FIBRE: 7g)
+            (_TOT_ KCAL: 767, PROT: 43g, CARBO: 104g, GRASSI: 24g, FIBRE: 6g)
         `;
     } else if (titolocontenuto_colazione_1.innerHTML === "MERENDA") {
         titolocontenuto_colazione_1.innerHTML = "PRANZO";
@@ -101,19 +101,19 @@ titolocontenuto_rightarrow_1.onclick = function() {
         titolocontenuto_colazione_1.innerHTML = "MERENDA";
         contenuto_default_1.innerHTML = `
             - 1 YOGURT 125g (2% grassi) | kcal: 77, prot: 10g, carbo: 15g, grassi: 3g<br>
-            - 2 ARANCE 230g(no buccia) | kcal: 100, prot: 2g, carbo: 30g, fibre: 5g<br>
+            - 2 PESCHE 280g | kcal: 110, prot: 2g, carbo: 26g, fibre: 4g<br>
             - 6 BISCOTTI (tremarie) 90g | kcal: 460, prot: 6g, carbo: 60g, grassi: 20g, fibre: 2g<br>
             - 1 INTEGRATORE PROT 1misurino | kcal: 120, prot: 25g, carbo: 3g, grassi: 1g<br>
-            (_TOT_ KCAL: 757, PROT: 43g, CARBO: 108g, GRASSI: 24g, FIBRE: 7g)
+            (_TOT_ KCAL: 767, PROT: 43g, CARBO: 104g, GRASSI: 24g, FIBRE: 6g)
         `;
     } else if (titolocontenuto_colazione_1.innerHTML === "MERENDA") {
         titolocontenuto_colazione_1.innerHTML = "CENA";
         contenuto_default_1.innerHTML = `
-            - BROCCOLI 150g | kcal: 52, prot: 5g, carbo: 10g, fibre: 5g<br>
+            - CAROTE 150g | kcal: 62, prot: 2g, carbo: 15g, fibre: 4g<br>
             - PETTO DI POLLO 200g | kcal: 330, prot: 62g, grassi: 7g<br>
             - 3 CUCCHIAI D'OLIO EVO | kcal: 390, grassi: 45g<br>
             - 1 YOGURT 125g | kcal: 90, prot: 5g, carbo: 10g, grassi: 3g<br>
-            (_TOT_ KCAL: 862, PROT: 72g, CARBO: 20g, GRASSI: 55g, FIBRE: 5g)
+            (_TOT_ KCAL: 872, PROT: 69g, CARBO: 25g, GRASSI: 55g, FIBRE: 4g)
         `;
     } else if (titolocontenuto_colazione_1.innerHTML === "CENA") {
         titolocontenuto_colazione_1.innerHTML = "COLAZIONE";
@@ -143,10 +143,10 @@ titolocontenuto_leftarrow_2.onclick = function() {
         titolocontenuto_colazione_2.innerHTML = "MERENDA";
         contenuto_default_2.innerHTML = `
             - 1 AVOCADO 200g | kcal: 480, prot: 9g, carbo: 4g, grassi: 46g<br>
-            - 2 ARANCE 230g(no buccia) | kcal: 100, prot: 2g, carbo: 30g, fibre: 5g<br>
+            - 2 PESCHE 280g | kcal: 110, prot: 2g, carbo: 26g, fibre: 4g<br>
             - 4 BISCOTTI (tremarie) 60g | kcal: 308, prot: 4g, carbo: 40g, grassi: 13g, fibre: 1g<br>
             - 1/2 INTEGRATORE PROT 1misurino | kcal: 60, prot: 13g, carbo: 1g<br>
-            (_TOT_ KCAL: 948, PROT: 28g, CARBO: 75g, GRASSI: 59g, FIBRE: 6g)
+            (_TOT_ KCAL: 958, PROT: 28g, CARBO: 71g, GRASSI: 59g, FIBRE: 5g)
         `;
     } else if (titolocontenuto_colazione_2.innerHTML === "MERENDA") {
         titolocontenuto_colazione_2.innerHTML = "PRANZO";
@@ -182,10 +182,10 @@ titolocontenuto_rightarrow_2.onclick = function() {
         titolocontenuto_colazione_2.innerHTML = "MERENDA";
         contenuto_default_2.innerHTML = `
             - 1 AVOCADO 200g | kcal: 480, prot: 9g, carbo: 4g, grassi: 46g<br>
-            - 2 ARANCE 230g(no buccia) | kcal: 100, prot: 2g, carbo: 30g, fibre: 5g<br>
+            - 2 PESCHE 280g | kcal: 110, prot: 2g, carbo: 26g, fibre: 4g<br>
             - 4 BISCOTTI (tremarie) 60g | kcal: 308, prot: 4g, carbo: 40g, grassi: 13g, fibre: 1g<br>
             - 1/2 INTEGRATORE PROT 1misurino | kcal: 60, prot: 13g, carbo: 1g<br>
-            (_TOT_ KCAL: 948, PROT: 28g, CARBO: 75g, GRASSI: 59g, FIBRE: 6g)
+            (_TOT_ KCAL: 958, PROT: 28g, CARBO: 71g, GRASSI: 59g, FIBRE: 5g)
         `;
     } else if (titolocontenuto_colazione_2.innerHTML === "MERENDA") {
         titolocontenuto_colazione_2.innerHTML = "CENA";
@@ -213,20 +213,20 @@ titolocontenuto_leftarrow_3.onclick = function() {
     if (titolocontenuto_colazione_3.innerHTML === "COLAZIONE") {
         titolocontenuto_colazione_3.innerHTML = "CENA";
         contenuto_default_3.innerHTML = `
-            - SPINACI 100g | kcal: 25, prot: 3g, carbo: 3g, fibre: 2g<br>
+            - CAROTE 100g | kcal: 41, prot: 1g, carbo: 10g, fibre: 3g<br>
             - PETTO DI POLLO 150g | kcal: 248, prot: 47g, grassi: 5g<br>
             - 3 CUCCHIAI D'OLIO EVO | kcal: 390, grassi: 45g<br>
             - PANE 100g | kcal: 200, prot: 10g, carbo: 50g, fibre: 2g<br>
-            (_TOT_ KCAL: 863, PROT: 60g, CARBO: 53g, GRASSI: 50g, FIBRE: 4g)
+            (_TOT_ KCAL: 879, PROT: 58g, CARBO: 60g, GRASSI: 50g, FIBRE: 5g)
         `;
     } else if (titolocontenuto_colazione_3.innerHTML === "CENA") {
         titolocontenuto_colazione_3.innerHTML = "MERENDA";
         contenuto_default_3.innerHTML = `
             - 1 YOGURT 125g (2% grassi) | kcal: 77, prot: 10g, carbo: 15g, grassi: 3g<br>
-            - 2 ARANCE 230g(no buccia) | kcal: 100, prot: 2g, carbo: 30g, fibre: 5g<br>
+            - 2 PESCHE 280g | kcal: 110, prot: 2g, carbo: 26g, fibre: 4g<br>
             - 6 BISCOTTI (tremarie) 90g | kcal: 460, prot: 6g, carbo: 60g, grassi: 20g, fibre: 2g<br>
             - 1 INTEGRATORE PROT 1misurino | kcal: 120, prot: 25g, carbo: 3g, grassi: 1g<br>
-            (_TOT_ KCAL: 757, PROT: 43g, CARBO: 108g, GRASSI: 24g, FIBRE: 7g)
+            (_TOT_ KCAL: 767, PROT: 43g, CARBO: 104g, GRASSI: 24g, FIBRE: 6g)
         `;
     } else if (titolocontenuto_colazione_3.innerHTML === "MERENDA") {
         titolocontenuto_colazione_3.innerHTML = "PRANZO";
@@ -259,19 +259,19 @@ titolocontenuto_rightarrow_3.onclick = function() {
         titolocontenuto_colazione_3.innerHTML = "MERENDA";
         contenuto_default_3.innerHTML = `
             - 1 YOGURT 125g (2% grassi) | kcal: 77, prot: 10g, carbo: 15g, grassi: 3g<br>
-            - 2 ARANCE 230g(no buccia) | kcal: 100, prot: 2g, carbo: 30g, fibre: 5g<br>
+            - 2 PESCHE 280g | kcal: 110, prot: 2g, carbo: 26g, fibre: 4g<br>
             - 6 BISCOTTI (tremarie) 90g | kcal: 460, prot: 6g, carbo: 60g, grassi: 20g, fibre: 2g<br>
             - 1 INTEGRATORE PROT 1misurino | kcal: 120, prot: 25g, carbo: 3g, grassi: 1g<br>
-            (_TOT_ KCAL: 757, PROT: 43g, CARBO: 108g, GRASSI: 24g, FIBRE: 7g)
+            (_TOT_ KCAL: 767, PROT: 43g, CARBO: 104g, GRASSI: 24g, FIBRE: 6g)
         `;
     } else if (titolocontenuto_colazione_3.innerHTML === "MERENDA") {
         titolocontenuto_colazione_3.innerHTML = "CENA";
         contenuto_default_3.innerHTML = `
-            - SPINACI 100g | kcal: 25, prot: 3g, carbo: 3g, fibre: 2g<br>
+            - CAROTE 100g | kcal: 41, prot: 1g, carbo: 10g, fibre: 3g<br>
             - PETTO DI POLLO 150g | kcal: 248, prot: 47g, grassi: 5g<br>
             - 3 CUCCHIAI D'OLIO EVO | kcal: 390, grassi: 45g<br>
             - PANE 100g | kcal: 200, prot: 10g, carbo: 50g, fibre: 2g<br>
-            (_TOT_ KCAL: 863, PROT: 60g, CARBO: 53g, GRASSI: 50g, FIBRE: 4g)
+            (_TOT_ KCAL: 879, PROT: 58g, CARBO: 60g, GRASSI: 50g, FIBRE: 5g)
         `;
     } else if (titolocontenuto_colazione_3.innerHTML === "CENA") {
         titolocontenuto_colazione_3.innerHTML = "COLAZIONE";
@@ -293,18 +293,18 @@ titolocontenuto_leftarrow_4.onclick = function() {
         contenuto_default_4.innerHTML = `
             - PETTO DI POLLO 150g | kcal: 248, prot: 47g, grassi: 5g<br>
             - 3 CUCCHIAI D'OLIO EVO | kcal: 390, grassi: 45g<br>
-            - 2 ARANCE 230g(no buccia) | kcal: 100, prot: 2g, carbo: 30g, fibre: 5g<br>
+            - 2 KIWI 180g | kcal: 80, prot: 1g, carbo: 9g, fibre: 2g<br>
             - 1 BANANA 100g | kcal: 90, prot: 1g, carbo: 22g<br>
-            (_TOT_ KCAL: 828, PROT: 50g, CARBO: 52g, GRASSI: 50g, FIBRE: 5g)
+            (_TOT_ KCAL: 808, PROT: 49g, CARBO: 31g, GRASSI: 50g, FIBRE: 2g)
         `;
     } else if (titolocontenuto_colazione_4.innerHTML === "CENA") {
         titolocontenuto_colazione_4.innerHTML = "MERENDA";
         contenuto_default_4.innerHTML = `
             - 1 YOGURT 125g (2% grassi) | kcal: 77, prot: 10g, carbo: 15g, grassi: 3g<br>
-            - 2 ARANCE 230g(no buccia) | kcal: 100, prot: 2g, carbo: 30g, fibre: 5g<br>
+            - 2 PESCHE 280g | kcal: 110, prot: 2g, carbo: 26g, fibre: 4g<br>
             - 6 BISCOTTI (tremarie) 90g | kcal: 460, prot: 6g, carbo: 60g, grassi: 20g, fibre: 2g<br>
             - 1 INTEGRATORE PROT 1misurino | kcal: 120, prot: 25g, carbo: 3g, grassi: 1g<br>
-            (_TOT_ KCAL: 757, PROT: 43g, CARBO: 108g, GRASSI: 24g, FIBRE: 7g)
+            (_TOT_ KCAL: 767, PROT: 43g, CARBO: 104g, GRASSI: 24g, FIBRE: 6g)
         `;
     } else if (titolocontenuto_colazione_4.innerHTML === "MERENDA") {
         titolocontenuto_colazione_4.innerHTML = "PRANZO";
@@ -336,19 +336,19 @@ titolocontenuto_rightarrow_4.onclick = function() {
         titolocontenuto_colazione_4.innerHTML = "MERENDA";
         contenuto_default_4.innerHTML = `
             - 1 YOGURT 125g (2% grassi) | kcal: 77, prot: 10g, carbo: 15g, grassi: 3g<br>
-            - 2 ARANCE 230g(no buccia) | kcal: 100, prot: 2g, carbo: 30g, fibre: 5g<br>
+            - 2 PESCHE 280g | kcal: 110, prot: 2g, carbo: 26g, fibre: 4g<br>
             - 6 BISCOTTI (tremarie) 90g | kcal: 460, prot: 6g, carbo: 60g, grassi: 20g, fibre: 2g<br>
             - 1 INTEGRATORE PROT 1misurino | kcal: 120, prot: 25g, carbo: 3g, grassi: 1g<br>
-            (_TOT_ KCAL: 757, PROT: 43g, CARBO: 108g, GRASSI: 24g, FIBRE: 7g)
+            (_TOT_ KCAL: 767, PROT: 43g, CARBO: 104g, GRASSI: 24g, FIBRE: 6g)
         `;
     } else if (titolocontenuto_colazione_4.innerHTML === "MERENDA") {
         titolocontenuto_colazione_4.innerHTML = "CENA";
         contenuto_default_4.innerHTML = `
             - PETTO DI POLLO 150g | kcal: 248, prot: 47g, grassi: 5g<br>
             - 3 CUCCHIAI D'OLIO EVO | kcal: 390, grassi: 45g<br>
-            - 2 ARANCE 230g(no buccia) | kcal: 100, prot: 2g, carbo: 30g, fibre: 5g<br>
+            - 2 KIWI 180g | kcal: 80, prot: 1g, carbo: 9g, fibre: 2g<br>
             - 1 BANANA 100g | kcal: 90, prot: 1g, carbo: 22g<br>
-            (_TOT_ KCAL: 828, PROT: 50g, CARBO: 52g, GRASSI: 50g, FIBRE: 5g)
+            (_TOT_ KCAL: 808, PROT: 49g, CARBO: 31g, GRASSI: 50g, FIBRE: 2g)
         `;
     } else if (titolocontenuto_colazione_4.innerHTML === "CENA") {
         titolocontenuto_colazione_4.innerHTML = "COLAZIONE";
@@ -396,8 +396,8 @@ titolocontenuto_leftarrow_5.onclick = function() {
         contenuto_default_5.innerHTML = `
             - 4 PANCAKE 160g (tremulini) | kcal: 472, prot: 10g, carbo: 58g, grassi: 23g, fibre: 2g<br>
             - 200ml LATTE (no lattosio) | kcal: 85, prot: 7g, carbo: 10g, grassi: 2g<br>
-            - 2 PESCHE 150g | kcal: 120, prot: 4g, carbo: 30g<br>
-            (_TOT_ KCAL: 677, PROT: 21g, CARBO: 98g, GRASSI: 25g, FIBRE: 2g)
+            - 2 PESCHE 280g | kcal: 110, prot: 2g, carbo: 26g, fibre: 4g<br>
+            (_TOT_ KCAL: 667, PROT: 19g, CARBO: 94g, GRASSI: 25g, FIBRE: 6g)
         `;
     }
 }
@@ -434,8 +434,8 @@ titolocontenuto_rightarrow_5.onclick = function() {
         contenuto_default_5.innerHTML = `
             - 4 PANCAKE 160g (tremulini) | kcal: 472, prot: 10g, carbo: 58g, grassi: 23g, fibre: 2g<br>
             - 200ml LATTE (no lattosio) | kcal: 85, prot: 7g, carbo: 10g, grassi: 2g<br>
-            - 2 PESCHE 150g | kcal: 120, prot: 4g, carbo: 30g<br>
-            (_TOT_ KCAL: 677, PROT: 21g, CARBO: 98g, GRASSI: 25g, FIBRE: 2g)
+            - 2 PESCHE 280g | kcal: 110, prot: 2g, carbo: 26g, fibre: 4g<br>
+            (_TOT_ KCAL: 667, PROT: 19g, CARBO: 94g, GRASSI: 25g, FIBRE: 6g)
         `;
     }
 }
@@ -466,9 +466,9 @@ titolocontenuto_leftarrow_6.onclick = function() {
         contenuto_default_6.innerHTML = `
             - PASTA (in bianco) 170g | kcal: 596, prot: 15g, carbo: 118g, grassi: 1g, fibre: 4g<br>
             - CECI 70g | kcal: 82, prot: 4g, carbo: 13g, grassi: 1g<br>
-            - 2 PESCHE 150g | kcal: 120, prot: 4g, carbo: 30g<br>
+            - 2 PESCHE 280g | kcal: 110, prot: 2g, carbo: 26g, fibre: 4g<br>
             - PANE 100g | kcal: 200, prot: 10g, carbo: 50g, fibre: 2g<br>
-            (_TOT_ KCAL: 998, PROT: 33g, CARBO: 211g, GRASSI: 2g, FIBRE: 6g)
+            (_TOT_ KCAL: 988, PROT: 31g, CARBO: 207g, GRASSI: 2g, FIBRE: 10g)
         `;
     } else if (titolocontenuto_colazione_6.innerHTML === "PRANZO") {
         titolocontenuto_colazione_6.innerHTML = "COLAZIONE";
@@ -487,9 +487,9 @@ titolocontenuto_rightarrow_6.onclick = function() {
         contenuto_default_6.innerHTML = `
             - PASTA (in bianco) 170g | kcal: 596, prot: 15g, carbo: 118g, grassi: 1g, fibre: 4g<br>
             - CECI 70g | kcal: 82, prot: 4g, carbo: 13g, grassi: 1g<br>
-            - 2 PESCHE 150g | kcal: 120, prot: 4g, carbo: 30g<br>
+            - 2 PESCHE 280g | kcal: 110, prot: 2g, carbo: 26g, fibre: 4g<br>
             - PANE 100g | kcal: 200, prot: 10g, carbo: 50g, fibre: 2g<br>
-            (_TOT_ KCAL: 998, PROT: 33g, CARBO: 211g, GRASSI: 2g, FIBRE: 6g)
+            (_TOT_ KCAL: 988, PROT: 31g, CARBO: 207g, GRASSI: 2g, FIBRE: 10g)
         `;
     } else if (titolocontenuto_colazione_6.innerHTML === "PRANZO") {
         titolocontenuto_colazione_6.innerHTML = "MERENDA";
